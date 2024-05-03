43f8d07037d0c23676ca3c67de32542645fb8563
hadsome-Droid <xkumbhax@gmail.com>
2024-05-03 20:20:50 +0300

    Create Card

---
+++ src/components/ui/card/card.tsx
@@ -0,0 +1,15 @@
+import { ComponentPropsWithRef, ReactNode } from 'react'
+
+import s from './card.module.scss'
+
+type Props = {
+  children: ReactNode
+} & ComponentPropsWithRef<'div'>
+
+export const Card = ({ children, ...rest }: Props) => {
+  return (
+    <div className={s.box} {...rest}>
+      {children}
+    </div>
+  )
+}
