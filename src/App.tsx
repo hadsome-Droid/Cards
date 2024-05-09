0d3237a424c38bf7473d889589d7983ca99edfb4
VladislavHrabko <velbel92@mail.ru>
2024-05-09 14:39:29 +0300

    added select without storybook, storybook doesn't work, css doesn't completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card/card'
+import SelectDemo from '@/components/ui/select/select'
 
 export function App() {
   return (
     <div>
-      sdf
       <Card>
         <Button>asdfadsf</Button>
       </Card>
+
+      <SelectDemo
+        category={'category'}
+        data={['sdfsd', 'sdgfsd', '23234', 's34sr5re5df']}
+        description={'what select'}
+      />
     </div>
   )
 }
