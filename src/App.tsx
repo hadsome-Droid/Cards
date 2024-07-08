f54a15eea4ad566d04aea380ea473eafbd9f190b
VladislavHrabko <velbel92@mail.ru>
2024-07-08 23:27:56 +0300

    Create page header with scss but without stories and adoptive scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card/card'
-import DropdownMenuDemo from '@/components/ui/dropDown/dropDown'
+import { DropdownMenuDemo } from '@/components/ui/dropDown/dropDown'
 
 export function App() {
   return (
     <div>
-      sdf
-      <Card>
-        <Button>asdfadsf</Button>
-      </Card>
-      <DropdownMenuDemo categoryOption={['1', '2', '3']} nameOfCategory={'numbers'} />
+      {/*sdf*/}
+      {/*<Card>*/}
+      {/*  <Button>asdfadsf</Button>*/}
+      {/*</Card>*/}
+      <DropdownMenuDemo categoryOption={['Learn', 'Edit', 'Delete']} />
     </div>
   )
 }
--- src/components/ui/dropDown/dropDown.tsx
+++ src/components/ui/dropDown/dropDown.tsx
@@ -12,10 +12,10 @@ import s from './dropDown.module.scss'
 
 type PropsType = {
   categoryOption: string[]
-  nameOfCategory: string
+  nameOfCategory?: string
 }
 
-const DropdownMenuDemo = ({ categoryOption, nameOfCategory }: PropsType) => {
+export const DropdownMenuDemo = ({ categoryOption, nameOfCategory }: PropsType) => {
   const [bookmarksChecked, setBookmarksChecked] = React.useState(true)
   const [urlsChecked, setUrlsChecked] = React.useState(false)
   const [person, setPerson] = React.useState('pedro')
@@ -30,79 +30,81 @@ const DropdownMenuDemo = ({ categoryOption, nameOfCategory }: PropsType) => {
 
       <DropdownMenu.Portal>
         <DropdownMenu.Content className={s.DropdownMenuContent} sideOffset={5}>
-          <DropdownMenu.Item className={s.DropdownMenuItem}>
-            New Tab <div className={s.RightSlot}>⌘+T</div>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item className={s.DropdownMenuItem}>
-            New Window <div className={s.RightSlot}>⌘+N</div>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item className={s.DropdownMenuItem} disabled>
-            New Private Window <div className={s.RightSlot}>⇧+⌘+N</div>
-          </DropdownMenu.Item>
-          <DropdownMenu.Sub>
-            <DropdownMenu.SubTrigger className={s.DropdownMenuSubTrigger}>
-              More Tools
-              <div className={s.RightSlot}>
-                <ChevronRightIcon />
-              </div>
-            </DropdownMenu.SubTrigger>
-            <DropdownMenu.Portal>
-              <DropdownMenu.SubContent
-                alignOffset={-5}
-                className={s.DropdownMenuSubContent}
-                sideOffset={2}
-              >
-                <DropdownMenu.Item className={s.DropdownMenuItem}>
-                  Save Page As… <div className={s.RightSlot}>⌘+S</div>
-                </DropdownMenu.Item>
-                <DropdownMenu.Item className={s.DropdownMenuItem}>
-                  Create Shortcut…
-                </DropdownMenu.Item>
-                <DropdownMenu.Item className={s.DropdownMenuItem}>Name Window…</DropdownMenu.Item>
-                {/*<DropdownMenu.Separator className={s.DropdownMenu.Separator} />*/}
-                <DropdownMenu.Separator className={s.DropdownMenu} />
-                <DropdownMenu.Item className={s.DropdownMenuItem}>
-                  Developer Tools
-                </DropdownMenu.Item>
-              </DropdownMenu.SubContent>
-            </DropdownMenu.Portal>
-          </DropdownMenu.Sub>
+          {/*<DropdownMenu.Item className={s.DropdownMenuItem}>*/}
+          {/*  New Tab <div className={s.RightSlot}>⌘+T</div>*/}
+          {/*</DropdownMenu.Item>*/}
+          {/*<DropdownMenu.Item className={s.DropdownMenuItem}>*/}
+          {/*  New Window <div className={s.RightSlot}>⌘+N</div>*/}
+          {/*</DropdownMenu.Item>*/}
+          {/*<DropdownMenu.Item className={s.DropdownMenuItem} disabled>*/}
+          {/*  New Private Window <div className={s.RightSlot}>⇧+⌘+N</div>*/}
+          {/*</DropdownMenu.Item>*/}
+          {/*<DropdownMenu.Sub>*/}
+          {/*  <DropdownMenu.SubTrigger className={s.DropdownMenuSubTrigger}>*/}
+          {/*    More Tools*/}
+          {/*    <div className={s.RightSlot}>*/}
+          {/*      <ChevronRightIcon />*/}
+          {/*    </div>*/}
+          {/*  </DropdownMenu.SubTrigger>*/}
+          {/*<DropdownMenu.Portal>*/}
+          {/*<DropdownMenu.SubContent*/}
+          {/*  alignOffset={-5}*/}
+          {/*  className={s.DropdownMenuSubContent}*/}
+          {/*  sideOffset={2}*/}
+          {/*>*/}
+          {/*<DropdownMenu.Item className={s.DropdownMenuItem}>*/}
+          {/*  Save Page As… <div className={s.RightSlot}>⌘+S</div>*/}
+          {/*</DropdownMenu.Item>*/}
+          {/*<DropdownMenu.Item className={s.DropdownMenuItem}>*/}
+          {/*  Create Shortcut…*/}
+          {/*</DropdownMenu.Item>*/}
+          {/*<DropdownMenu.Item className={s.DropdownMenuItem}>Name Window…</DropdownMenu.Item>*/}
+          {/*/!*<DropdownMenu.Separator className={s.DropdownMenu.Separator} />*!/*/}
+          {/*<DropdownMenu.Separator className={s.DropdownMenu} />*/}
+          {/*<DropdownMenu.Item className={s.DropdownMenuItem}>*/}
+          {/*  Developer Tools*/}
+          {/*</DropdownMenu.Item>*/}
+          {/*</DropdownMenu.SubContent>*/}
+          {/*</DropdownMenu.Portal>*/}
+          {/*</DropdownMenu.Sub>*/}
 
-          <DropdownMenu.Separator className={s.DropdownMenuSeparator} />
+          {/*<DropdownMenu.Separator className={s.DropdownMenuSeparator} />*/}
 
-          <DropdownMenu.CheckboxItem
-            checked={bookmarksChecked}
-            className={s.DropdownMenuCheckboxItem}
-            onCheckedChange={setBookmarksChecked}
-          >
-            <DropdownMenu.ItemIndicator className={s.DropdownMenuItemIndicator}>
-              <CheckIcon />
-            </DropdownMenu.ItemIndicator>
-            Show Bookmarks <div className={s.RightSlot}>⌘+B</div>
-          </DropdownMenu.CheckboxItem>
-          <DropdownMenu.CheckboxItem
-            checked={urlsChecked}
-            className={s.DropdownMenuCheckboxItem}
-            onCheckedChange={setUrlsChecked}
-          >
-            <DropdownMenu.ItemIndicator className={s.DropdownMenuItemIndicator}>
-              <CheckIcon />
-            </DropdownMenu.ItemIndicator>
-            Show Full URLs
-          </DropdownMenu.CheckboxItem>
+          {/*<DropdownMenu.CheckboxItem*/}
+          {/*  checked={bookmarksChecked}*/}
+          {/*  className={s.DropdownMenuCheckboxItem}*/}
+          {/*  onCheckedChange={setBookmarksChecked}*/}
+          {/*>*/}
+          {/*  <DropdownMenu.ItemIndicator className={s.DropdownMenuItemIndicator}>*/}
+          {/*    <CheckIcon />*/}
+          {/*  </DropdownMenu.ItemIndicator>*/}
+          {/*  Show Bookmarks <div className={s.RightSlot}>⌘+B</div>*/}
+          {/*</DropdownMenu.CheckboxItem>*/}
+          {/*<DropdownMenu.CheckboxItem*/}
+          {/*  checked={urlsChecked}*/}
+          {/*  className={s.DropdownMenuCheckboxItem}*/}
+          {/*  onCheckedChange={setUrlsChecked}*/}
+          {/*>*/}
+          {/*  <DropdownMenu.ItemIndicator className={s.DropdownMenuItemIndicator}>*/}
+          {/*    <CheckIcon />*/}
+          {/*  </DropdownMenu.ItemIndicator>*/}
+          {/*  Show Full URLs*/}
+          {/*</DropdownMenu.CheckboxItem>*/}
 
-          <DropdownMenu.Separator className={s.DropdownMenuSeparator} />
+          {/*<DropdownMenu.Separator className={s.DropdownMenuSeparator} />*/}
 
           <DropdownMenu.Label className={s.DropdownMenuLabel}>{nameOfCategory}</DropdownMenu.Label>
           <DropdownMenu.RadioGroup onValueChange={setPerson} value={person}>
             {categoryOption.map(el => (
               <DropdownMenu.RadioItem className={s.DropdownMenuRadioItem} value={el}>
-                <DropdownMenu.ItemIndicator className={s.DropdownMenuItemIndicator}>
-                  <DotFilledIcon />
-                </DropdownMenu.ItemIndicator>
+                {/*<DropdownMenu.ItemIndicator className={s.DropdownMenuItemIndicator}>*/}
+                {/*  <DotFilledIcon />*/}
+                {/*</DropdownMenu.ItemIndicator>*/}
                 {el}
               </DropdownMenu.RadioItem>
             ))}
+
+            <DropdownMenu.Separator className={s.DropdownMenuSeparator} />
           </DropdownMenu.RadioGroup>
 
           <DropdownMenu.Arrow className={s.DropdownMenuArrow} />
@@ -111,5 +113,3 @@ const DropdownMenuDemo = ({ categoryOption, nameOfCategory }: PropsType) => {
     </DropdownMenu.Root>
   )
 }
-
-export default DropdownMenuDemo
